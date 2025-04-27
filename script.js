7722851756e0b48af1f68e02b8717c0999bba122
alisomers <alisomers418@hotmail.com>
2025-04-27 16:42:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-
-// script.js
-
 function toggleContent(id) {
   const content = document.getElementById(id);
-  if (content) {
-    content.classList.toggle('active');
+  if (content.style.display === "block") {
+    content.style.display = "none";
+  } else {
+    document.querySelectorAll('.content').forEach(function(div) {
+      div.style.display = 'none';
+    });
+    content.style.display = "block";
   }
 }
-
